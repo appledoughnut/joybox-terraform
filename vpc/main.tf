08fd670377ca0d8119fd870f181a9a28564a515c
Jonghyeok Lee <>
2022-09-19 22:47:08 +0900

    gateway, route, nat gateway 추가

--- vpc/main.tf
+++ vpc/main.tf
@@ -37,3 +37,61 @@ resource "aws_subnet" "private_subnet" {
     }
 }
 
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags = {
+    "Name" = "internet gateway"
+  }
+}
+
+resource "aws_route" "route_to_gateway" {
+  route_table_id = "${aws_vpc.vpc.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.internet_gateway.id}"
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+  depends_on = [aws_internet_gateway.internet_gateway]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${aws_eip.nat_eip.id}"
+  subnet_id = "${aws_subnet.public_subnet.id}"
+  depends_on = [
+    aws_internet_gateway.internet_gateway
+  ]
+}
+
+resource "aws_route_table" "private_route_table" { 
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags = {
+    "Name" = "private route table"
+  }
+}
+
+resource "aws_route" "route_to_nat" {
+  route_table_id = "${aws_route_table.private_route_table.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = "${aws_nat_gateway.nat.id}"
+}
+
+resource "aws_default_security_group" "security_group" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    protocol = -1
+    self = true
+    from_port = 0
+    to_port = 0
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
