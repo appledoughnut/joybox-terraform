f770923a6dc76eb248ca12a63cba5c4f0027e330
jonghoon <>
2022-09-20 22:20:57 +0900

    public subnet 에 public ip 가 할당되도록 수정

--- vpc/main.tf
+++ vpc/main.tf
@@ -21,6 +21,7 @@ resource "aws_subnet" "public_subnet" {
     vpc_id = "${aws_vpc.vpc.id}"
     cidr_block = "10.10.1.0/24"
     availability_zone = "${data.aws_availability_zone.az.name}"
+    map_public_ip_on_launch = true
 
     tags = {
       "Name" = "public subnet"
