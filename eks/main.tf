a309b3fca92eb213e2c6a53dbe76c830db6d8ca2
jonghoon <>
2022-09-25 13:58:08 +0900

    ALB 를 위한 iam policy 추가

--- eks/main.tf
+++ eks/main.tf
@@ -20,7 +20,16 @@ module "eks" {
         }
     }
 
+    iam_role_additional_policies = [aws_iam_policy.worker_policy.arn]
+
     tags = {
         Environment = "dev"
     }
+}
+
+resource "aws_iam_policy" "worker_policy" {
+    name = "worker-policy"
+    description = "Worker policy for the ALB Ingress"
+
+    policy = file("iam-policy.json")
 }
