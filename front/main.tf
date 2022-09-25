a102e03f1679452ed66720e4c684187cca4efe17
Jonghyeok Lee <>
2022-09-25 18:32:33 +0900

    위치 변경 및 ECR 설정 추가

---
+++ front/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  
+}
+
+resource "aws_ecr_repository" "front" {
+  name                 = "front"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
