e2bc152d34244422a8a92d24cfb483fd021d28a4
jonghoon <>
2022-10-10 08:53:23 +0900

    joybox-app 폴더 삭제

--- env/helm-charts/main.tf
+++ env/helm-charts/main.tf
@@ -2,9 +2,3 @@ resource "aws_s3_bucket" "helm_charts" {
   bucket = "joybox-helm-charts"
   acl = "private"
 }
-
-resource "aws_s3_bucket_object" "joybox_app" {
-    bucket = "${aws_s3_bucket.helm_charts.id}"
-    key    = "joybox-app/"
-    content_type = "application/x-directory"
-}
