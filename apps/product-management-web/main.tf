887015fa7164ab9edbe7e3d62947ddfd33f48f4d
jonghoon <>
2022-10-30 14:23:34 +0900

    AWS 리소스 삭제

--- apps/product-management-web/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "product-management-web" {
-  name                 = "product-management-web"
-  image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
--- apps/vendor-client-gateway/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "vendor-client-gateway" {
-  name                 = "vendor-client-gateway"
-  image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
